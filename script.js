4a77844caa68b36273c5239319d236847cd98236
Ryan Cepeda <ryancepeda2@gmail.com>
2024-06-26 20:20:16 -0400

    Create grid of 16x16 and prompt button for dimensions

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const container = document.querySelector("#container");
-for(let i = 0;i<256;i++){
-  const square = document.createElement("div");
-  square.setAttribute("style", "height:90px; width:90px; background:red;display:flex; flex-direction:column;");
-  container.append(square);
+function createGrid(size){
+  gridSize = size * size;
+  gridDim = 960/size;
+  for(let i = 0;i<gridSize;i++){
+    const square = document.createElement("div");
+    square.setAttribute("style", "height:60px; width:60px; background:red;display:flex; flex-direction:column; border:0px solid black;");
+    square.addEventListener('mouseover',()=>{
+      square.style.backgroundColor = 'blue';
+    })
+    square.addEventListener('mouseout',()=>{
+      square.style.backgroundColor = "red";
+    })
+    container.append(square);
+  }
 }
+
+
+const gridBtn = document.querySelector("#gridBtn");
+var gridInput = 0;
+function showPrompt(){
+  gridInput = prompt("Enter dimensions");
+
+}
+gridBtn.addEventListener('click',showPrompt);
+
+
