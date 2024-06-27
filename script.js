c438d96006eb62cc3c88b456f9f30b5d1e3650df
Ryan Cepeda <ryancepeda2@gmail.com>
2024-06-27 13:29:35 -0400

    new grid is created, but old one still exists

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function createGrid(size){
   gridSize = size * size;
   gridDim = 960/size;
   for(let i = 0;i<gridSize;i++){
-    const square = document.createElement("div");
-    square.setAttribute("style", "height:60px; width:60px; background:red;display:flex; flex-direction:column; border:0px solid black;");
+    const square = document.createElement("div"); //when using template literals, you have to use backticks ` not single quotes '
+    square.setAttribute("style", `height:${gridDim}px; width:${gridDim}px; background:red;display:flex; flex-direction:column; border:0px solid black;`);
     square.addEventListener('mouseover',()=>{
       square.style.backgroundColor = 'blue';
     })
@@ -12,16 +12,15 @@ function createGrid(size){
       square.style.backgroundColor = "red";
     })
     container.append(square);
+    }
   }
-}
-
 
 const gridBtn = document.querySelector("#gridBtn");
-var gridInput = 0;
-function showPrompt(){
-  gridInput = prompt("Enter dimensions");
 
+function showPrompt(){
+  let gridInput = parseInt(prompt("Enter dimensions"));
+  
+  createGrid(gridInput);
 }
-gridBtn.addEventListener('click',showPrompt);
 
 
