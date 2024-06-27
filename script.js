ad6f912bfd8c3325bf6f89e7b9a6bb242dc8064d
Ryan Cepeda <ryancepeda2@gmail.com>
2024-06-27 16:16:55 -0400

    added erase button and random color picker button and return to black and white button

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const container = document.querySelector("#container");
+let mouseDown = false
+let greyscale = true;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+createGrid(16);
 function createGrid(size){
   gridSize = size * size;
   const computedStyle = window.getComputedStyle(container);
   const boardDim = computedStyle.getPropertyValue('width');
-
   gridDim = 500/size;
   console.log(boardDim);
   for(let i = 0;i<gridSize;i++){
     const square = document.createElement("div"); //when using template literals, you have to use backticks ` not single quotes '
-    square.setAttribute("style", `height:${gridDim}px; width:${gridDim}px; background:red;display:flex; flex-direction:column; border:0px solid black;`);
+    square.setAttribute("style", `height:${gridDim}px; width:${gridDim}px; background:white;display:flex; flex-direction:column; border:0px solid black;`);
     square.addEventListener('mouseover',()=>{
-      square.style.backgroundColor = 'blue';
-    })
-    square.addEventListener('mouseout',()=>{
-      square.style.backgroundColor = "red";
+      if(!mouseDown){
+        return;
+      }
+      else if (greyscale){
+        square.style.backgroundColor = 'black';
+      }
+      else{
+        const R = Math.floor(Math.random() * 256);
+        const G = Math.floor(Math.random() * 256);
+        const B = Math.floor(Math.random() * 256);
+        square.style.backgroundColor = `rgb(${R},${G},${B})`
+      }
     })
     container.append(square);
     }
@@ -29,5 +41,22 @@ function showPrompt(){
   container.replaceChildren();
   createGrid(gridInput);
 }
+
+function erase(){
+  const childDivs = container.querySelectorAll('div');
+  for(const square of childDivs){
+  square.style.backgroundColor = 'white';
+  }
+}
+
+function color(){
+  greyscale = false;
+}
+
+function blackwhite(){
+  greyscale = true;
+}
+
+
 
 
