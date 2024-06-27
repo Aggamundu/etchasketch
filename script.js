15ae05de97f2035cb4c1174ba2329807e987479a
Ryan Cepeda <ryancepeda2@gmail.com>
2024-06-27 14:02:46 -0400

    new grid created when button is clicked styles added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const container = document.querySelector("#container");
 function createGrid(size){
   gridSize = size * size;
-  gridDim = 960/size;
+  const computedStyle = window.getComputedStyle(container);
+  const boardDim = computedStyle.getPropertyValue('width');
+
+  gridDim = 500/size;
+  console.log(boardDim);
   for(let i = 0;i<gridSize;i++){
     const square = document.createElement("div"); //when using template literals, you have to use backticks ` not single quotes '
     square.setAttribute("style", `height:${gridDim}px; width:${gridDim}px; background:red;display:flex; flex-direction:column; border:0px solid black;`);
@@ -18,8 +22,11 @@ function createGrid(size){
 const gridBtn = document.querySelector("#gridBtn");
 
 function showPrompt(){
-  let gridInput = parseInt(prompt("Enter dimensions"));
-  
+  let gridInput = parseInt(prompt("Enter dimensions from [0,100)"));
+    if(gridInput>100 || gridInput<=0){
+      showPrompt();
+    }
+  container.replaceChildren();
   createGrid(gridInput);
 }
 
